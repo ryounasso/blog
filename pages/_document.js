0037f33d7301e27136d735f74761c52d139f7ffd
ryonasso <ryounaoyuki@gmail.com>
2021-02-10 14:15:50 +0900

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -6,28 +6,6 @@ class MyDocument extends Document {
             <Html lang="ja">
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
-
-                    { /* gtag / Google Analytics を利用する */}
-          {
-            GA_TRACKING_ID && <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-            />
-          }
-          {
-            GA_TRACKING_ID && <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_TRACKING_ID}', {
-                    page_path: window.location.pathname,
-                  });
-                `,
-              }}
-            />
-          }
                 </Head>
                 <body>
                     <Main />
