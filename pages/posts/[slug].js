95ac3a0cb8f9eb590f479fd64108cbff06e355e4
ryonasso <ryounaoyuki@gmail.com>
2021-08-22 21:11:13 +0900

    fix : エラーの修正

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -13,7 +13,10 @@ export default function Post(params) {
     <>
       <Head>
         <title>{params.title}</title>
-        <meta property="og:image" content={props.ogImageUrl} />
+        <meta
+          property="og:image"
+          content="https://og-image-ryounasso.vercel.app/%E3%82%B5%E3%82%A4%E3%83%9C%E3%82%A6%E3%82%BA%E3%81%95%E3%82%93%E3%81%AE%E3%82%A4%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%83%E3%83%97%E3%81%AB%E5%8F%82%E5%8A%A0%E3%81%97%E3%81%A6%E3%81%8D%E3%81%BE%E3%81%97%E3%81%9F%EF%BC%81%EF%BC%81.png?theme=dark&md=1&fontSize=100px"
+        />
       </Head>
       <Layout title={params.title}>
         <div className="post-meta">
@@ -32,18 +35,10 @@ export default function Post(params) {
 
 export async function getStaticProps({ params }) {
   const content = await readContentFile({ fs, slug: params.slug });
-  const blogPostUrl = [process.env.CMS_API_URL, params.id].join("/");
-  const response = await fetch(blogPostUrl, { headers });
-  const { title } = await response.json();
-  const baseUrl = {
-    production: "https://tdkn.dev",
-    development: "https://localhost:3000",
-  }[process.env.NODE_ENV];
 
   return {
     props: {
       ...content,
-      ogImageUrl: `${baseUrl}/api/ogp?title=${title}`,
     },
   };
 }
@@ -56,8 +51,6 @@ export async function getStaticPaths() {
       slug: path.parse(filename).name,
     },
   }));
-  const response = await fetch(process.env.CMS_API_URL, { headers });
-  const { contents: posts } = await response.json();
 
   return { paths, fallback: false };
 }
