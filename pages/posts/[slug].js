9c091fe48e3aa4b8dce68fe379cbb8ef8291dd61
ryonasso <ryounaoyuki@gmail.com>
2021-09-14 17:33:32 +0900

    fix : metaタグの修正

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -9,20 +9,22 @@ import Head from "next/head";
 const headers = { "X-API-KEY": process.env.CMS_API_KEY };
 
 export default function Post(params) {
+  const content =
+    "https://og-image-ryounasso.vercel.app/" +
+    params.title +
+    ".png?theme=dark&md=1&fontSize=100px";
   return (
     <>
       <Head>
         <title>{params.title}</title>
         <meta
           property="og:image"
-          content="https://og-image-ryounasso.vercel.app/%E3%82%B5%E3%82%A4%E3%83%9C%E3%82%A6%E3%82%BA%E3%81%95%E3%82%93%E3%81%AE%E3%82%A4%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%83%E3%83%97%E3%81%AB%E5%8F%82%E5%8A%A0%E3%81%97%E3%81%A6%E3%81%8D%E3%81%BE%E3%81%97%E3%81%9F%EF%BC%81%EF%BC%81.png?theme=dark&md=1&fontSize=100px"
+          // content="https://og-image-ryounasso.vercel.app/%E3%82%B5%E3%82%A4%E3%83%9C%E3%82%A6%E3%82%BA%E3%81%95%E3%82%93%E3%81%AE%E3%82%A4%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%83%E3%83%97%E3%81%AB%E5%8F%82%E5%8A%A0%E3%81%97%E3%81%A6%E3%81%8D%E3%81%BE%E3%81%97%E3%81%9F%EF%BC%81%EF%BC%81.png?theme=dark&md=1&fontSize=100px"
+          content={content}
         />
         <meta name="og:title" content={params.title} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:image"
-          content={`https://og-image-ryounasso.vercel.app/{params.title}.png?theme=dark&md=1&fontSize=100px`}
-        />
+        <meta property="twitter:image" content={content} />
         <meta name="twitter:title" content={params.title} />
       </Head>
       <Layout title={params.title}>
